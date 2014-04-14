674ccbfc9c068847a2377a2e066ee4efb6155dd5
ralon039 <ralon039@fiu.edu>
2014-04-14 01:18:46 -0400

    main, Search, carDetails, css, js
    minor changes in main, mostly html
    Search running
    carDetails im still working on that
    included my css and js folders

--- carDetails.php
+++ carDetails.php
@@ -5,77 +5,88 @@ include "methods.php";
 include "header.php";
 ?>
 
-<table border='0' width='100%' name='orgStudentDemographics'>
-	<tr>
-		<td colspan='5' align='center'><h3>"Car Details"</h3></td>
-	</tr>
-	<tr>
-		<td width='3%'></td>
-		<td width='350' height='300' align='center' valign='middle'>
-			<img src='' alt='No Image Available'/>
-			<?php
-			if(isset($_POST['editDemo'])){echo "<br/><div align='right'><font size='1'><a href='#'>Add Img...</a></font></div>";}
-			else{}
-			?>						
-		</td>
-		<td width='3%'></td>
-		<td valign='top'>
-			<fieldset class='divFormatHeader'><legend>Specifics</legend>
-			<table border='0' width='100%' name='orgCarDetails' style='font-size:12'>
-				<tr>
-					<td>
-						<b>Make:</b>
-					</td>
-					<td width='30%'>
-						Honda
-					</td>
-					<td>
-						<b>Color:</b>
-					</td>
-					<td width='30%'>
-						RED
-					</td>
-				</tr>
-				<tr>
-					<td>
-						<b>Model:</b>
-					</td>
-					<td>
-						xls2130
-					</td>
-					<td>
-						<b>Miles:</b>
-					</td>
-					<td>
-						500,000
-					</td>
-				</tr>
-				<tr>
-					<td>
-						<b>Year:</b>
-					</td>
-					<td>
-						2006
-					</td>
-					
-				</tr>
-			</table>
-		</td>
-		<td width='3%'></td>
-	</tr>
-	<tr>
-		<td colspan='5'>PICTURES:
-			<br/><br/>
-		</td>
-	</tr>
-	<tr>
-		<td colspan='5'>Additional Details:
-			<br/><br/>
-		</td>
-	</tr>
-</table>
 
+<html>
+<body>
+	<link href = "css/bootstrap.min.css" rel = "stylesheet">
+	<link rel="stylesheet" href="style.css" type="text/css" />
+	<table border='0' width='100%' name='carDetailsTable'>
+		<th>
+			<td colspan='5' align='center'><h3 class = "panel-heading">Car Details</h3></td>
+		</th>
+		<tr>
+			<td width='3%'></td>
+			<td width='350' height='300' align='center' valign='middle'>
+				<img src='' alt='No Image Available' class="img-rounded"/>
+				<?php
+					var_dump($_POST['carDetails']);
+					if(isset($_POST['carDetails']))
+						{
+							echo "<img src='' alt='No Image Available' class='img-rounded'/>";
+							echo "<br/><div align='right'><font size='1'><a href='#'>Add Img...</a></font></div>";
+						}
+					else{echo "Request method not supported";}
+				?>						
+			</td>
+			<td width='3%'></td>
+			<td valign='top'>
+				<fieldset class='divFormatHeader'><legend>Vehicle Specifications</legend>
+				<table border='0' width='100%' name='orgCarDetails' style='font-size:12'>
+					<tr>
+						<td>
+							<b>Make:</b>
+						</td>
+						<td width='30%'>
+							Honda
+						</td>
+						<td>
+							<b>Color:</b>
+						</td>
+						<td width='30%'>
+							RED
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<b>Model:</b>
+						</td>
+						<td>
+							xls2130
+						</td>
+						<td>
+							<b>Miles:</b>
+						</td>
+						<td>
+							500,000
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<b>Year:</b>
+						</td>
+						<td>
+							2006
+						</td>
+						
+					</tr>
+				</table>
+			</td>
+			<td width='3%'></td>
+		</tr>
+		<tr>
+			<td colspan='5'>PICTURES:
+				<br/><br/>
+			</td>
+		</tr>
+		<tr>
+			<td colspan='5'>Additional Details:
+				<br/><br/>
+			</td>
+		</tr>
+	</table>
 
+</body>
+</html>
 
 <?php
 include "footer.php";
