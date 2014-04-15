80afcbe5e9d29ba28ce6f7c3ebafec88fb94ca10
ralon039 <ralon039@fiu.edu>
2014-04-14 20:10:05 -0400

    search, car details

--- Search.php
+++ Search.php
@@ -1,80 +1,150 @@
-<?php
-include "methods.php";
+<!DOCTYPE html>
 
-//From Search button @ main.php
-if(isset($_POST['search'])){
 
-	//Initiate connection to the database
-	$link = mysql_connect($dbLocalhost, $dbUser, $dbPw);
-	if (!$link) {die('Not connected : ' . mysql_error());}
-	$db_selected = mysql_select_db($dbDb, $link);
 
-	//var_dump($_POST);
-	$carPriceMax = intval($_POST['price']);  
-
-	//var_dump ($carPriceMax);
-
-	$carList = "";
-	
-	//Search by VIN ONLY
-	if($_POST['id_vin'] != '')
-		{
-			$carList=mysql_query("SELECT car_id, car.make_id, car.model_id, year, color, price, status_id, make, model FROM car
-									INNER JOIN make_id ON car.make_id = make_id.make_id
-									INNER JOIN model_id ON car.model_id = model_id.model_id
-									WHERE status_id=1 AND id_vin=".$_POST['id_vin']."");
-			if($carList == NULL) {$carListErr = " No VIN could be found";}
-		}
-	else //Default search parameters No inout on year/make/model/color
-		{
-			$carList=mysql_query("SELECT car_id, car.make_id, car.model_id, year, color, price, status_id, make, model FROM car
-									INNER JOIN make_id ON car.make_id = make_id.make_id
-									INNER JOIN model_id ON car.model_id = model_id.model_id
-									WHERE status_id=1 AND price <".$carPriceMax."
-									ORDER BY price");
-		}
-		
-		if($_POST['year'] != '')//Search by year
-		{	
-			$carList=mysql_query("SELECT car_id, car.make_id, car.model_id, year, color, price, status_id, make, model FROM car
-									INNER JOIN make_id ON car.make_id = make_id.make_id
-									INNER JOIN model_id ON car.model_id = model_id.model_id
-									WHERE status_id=1 AND year=".$_POST['year']."
-									ORDER BY price");
-		}
-		if($_POST['make'] != '')//Search by make and organize by model
-		{
-			$carList=mysql_query("SELECT car_id, car.make_id, car.model_id, year, color, price, status_id, make, model FROM car
-									INNER JOIN make_id ON car.make_id = make_id.make_id
-									INNER JOIN model_id ON car.model_id = model_id.model_id
-									WHERE status_id=1 AND make=".$_POST['make']."
-									ORDER BY model");
-		}
-		if($_POST['model'] != '')//Search by model
-		{
-			$carList=mysql_query("SELECT car_id, car.make_id, car.model_id, year, color, price, status_id, make, model FROM car
-									INNER JOIN make_id ON car.make_id = make_id.make_id
-									INNER JOIN model_id ON car.model_id = model_id.model_id
-									WHERE status_id=1 AND model=".$_POST['model']."
-									ORDER BY price");
-		}
-		if($_POST['color'] != '')//Search by color
-		{
-			$carList=mysql_query("SELECT car_id, car.make_id, car.model_id, year, color, price, status_id, make, model FROM car
-									INNER JOIN make_id ON car.make_id = make_id.make_id
-									INNER JOIN model_id ON car.model_id = model_id.model_id
-									WHERE status_id=1 AND color=".$_POST['color']."
-									ORDER BY price");
-		}//End of search
+<html>
+	<head>
+		<div class="panel panel-default">
+			<div class="panel-heading" align = "center">Search Results</div>
+			<div class="panel-body">
+				<p>User Search parameters goes here</p>
+			</div>
+	</head>
+	<body>
+		<link href = "css/bootstrap.min.css" rel = "stylesheet">
+		<table class = "table table-hover" >
 			
-	
-		
-	while($carListDB=mysql_fetch_array($carList)){
-		//Show table in HTML Code with Buy Button
-	}
-	
-	//Close connection to DB
-	mysql_close($link);
-}
+			<tbody>
+				<?php
+				
+					
+					include "methods.php";
 
-?>
+					//From Search button @ main.php
+					if(isset($_POST['search'])){
+
+						//Initiate connection to the database
+						$link = mysql_connect($dbLocalhost, $dbUser, $dbPw);
+						if (!$link) {die('Not connected : ' . mysql_error());}
+						$db_selected = mysql_select_db($dbDb, $link);
+
+						
+						$carPriceMax = intval($_POST['price']);  
+												
+						//Search by VIN ONLY
+						if($_POST['id_vin'] != '')
+							{
+								$carList=mysql_query("SELECT car_id, car.make_id, car.model_id, year, color, price, status_id, condition_id, mileage, make, model, car_condition_name FROM car
+														INNER JOIN make_id ON car.make_id = make_id.make_id
+														INNER JOIN model_id ON car.model_id = model_id.model_id
+														INNER JOIN car_condition ON car.condition_id = car_condition.id_car_condition
+														WHERE status_id=1 AND id_vin='".$_POST['id_vin']."'");
+								if($carList == NULL) {$carListErr = " No VIN could be found";}
+							}
+						else //Default search parameters No input on year/make/model/color
+							{
+								if($carPriceMax <= 50000)
+									{
+										$carList=mysql_query("SELECT car_id, car.make_id, car.model_id, year, color, price, status_id, condition_id, mileage, make, model, car_condition_name FROM car
+															INNER JOIN make_id ON car.make_id = make_id.make_id
+															INNER JOIN model_id ON car.model_id = model_id.model_id
+															INNER JOIN car_condition ON car.condition_id = car_condition.id_car_condition
+															WHERE status_id=1 AND price <".$carPriceMax."
+															ORDER BY price");
+									}
+								
+								else
+									{
+										$carList=mysql_query("SELECT car_id, car.make_id, car.model_id, year, color, price, status_id, condition_id, mileage, make, model, car_condition_name FROM car
+														INNER JOIN make_id ON car.make_id = make_id.make_id
+														INNER JOIN model_id ON car.model_id = model_id.model_id
+														INNER JOIN car_condition ON car.condition_id = car_condition.id_car_condition
+														WHERE status_id=1 AND price >".$carPriceMax."
+														ORDER BY price");
+									}
+							}
+							
+							if($_POST['year'] != '')//Search by year
+							{	
+								$carList=mysql_query("SELECT car_id, car.make_id, car.model_id, year, color, price, car.status_id, condition_id, mileage, make, model, car_condition_name FROM car
+														INNER JOIN make_id ON car.make_id = make_id.make_id
+														INNER JOIN model_id ON car.model_id = model_id.model_id
+														INNER JOIN car_condition ON car.condition_id = car_condition.id_car_condition
+														WHERE status_id=1 AND year='".$_POST['year']."'
+														ORDER BY price");
+							}
+							if($_POST['make'] != '')//Search by make and organize by model
+							{
+								$carList=mysql_query("SELECT car_id, car.make_id, car.model_id, year, color, price, status_id, condition_id, mileage, make, model, car_condition_name FROM car
+														INNER JOIN make_id ON car.make_id = make_id.make_id
+														INNER JOIN model_id ON car.model_id = model_id.model_id
+														INNER JOIN car_condition ON car.condition_id = car_condition.id_car_condition
+														WHERE status_id=1 AND make='".$_POST['make']."'
+														ORDER BY model");
+							}
+							if($_POST['model'] != '')//Search by model
+							{
+								$carList=mysql_query("SELECT car_id, car.make_id, car.model_id, year, color, price, status_id, condition_id, mileage, make, model, car_condition_name FROM car
+														INNER JOIN make_id ON car.make_id = make_id.make_id
+														INNER JOIN model_id ON car.model_id = model_id.model_id
+														INNER JOIN car_condition ON car.condition_id = car_condition.id_car_condition
+														WHERE status_id=1 AND model='".$_POST['model']."'
+														ORDER BY price");
+							}
+							if($_POST['color'] != '')//Search by color
+							{
+								$carList=mysql_query("SELECT car_id, car.make_id, car.model_id, year, color, price, status_id, condition_id, mileage, make, model, car_condition_name FROM car
+														INNER JOIN make_id ON car.make_id = make_id.make_id
+														INNER JOIN model_id ON car.model_id = model_id.model_id
+														INNER JOIN car_condition ON car.condition_id = car_condition.id_car_condition
+														WHERE status_id=1 AND color='".$_POST['color']."'
+														ORDER BY price");
+							}//End of search
+					
+						$line = 0;
+						//$buyButton = "<button type="button" class="btn btn-primary btn-lg btn-block">Buy</button>";
+						while($carListDB=mysql_fetch_array($carList)){						
+							
+							if($line == 0)
+							{
+								echo "
+									<thead class = 'table table-hover'>
+										<tr>
+											<td>#</td>
+											<td>Year/Make/Model</td>
+											<td>Price</td>
+											<td>Condition/Milage</td>
+											<td></td>
+										</tr>
+									</thead>";
+							}
+							$line ++;
+						
+							//Show table in HTML Code with Buy Button	
+							echo "
+								<form role='form' method = 'POST' action = 'carDetails.php' target = 'carDetails'>
+									<tr>
+										<td>".$line."</td>
+										<td>".$carListDB['year']." ".$carListDB['make']." ".$carListDB['model']."</td>
+										<td>".$carListDB['price']."</td>
+										<td>".$carListDB['car_condition_name']." ".$carListDB['mileage']." miles </td>
+										<td>
+											<input  type='submit' name='carDetails".$carListDB['car_id']."' value = 'Car Details' class='btn btn-primary btn-lg btn-block' >
+											
+										</td>
+									</tr>
+								</form>";
+						
+					}
+					
+					//Close connection to DB
+					mysql_close($link);
+					
+	}
+				?>
+				
+			</tbody>
+
+		</table>
+	</body>
+</html>
--- carDetails.php
+++ carDetails.php
@@ -6,6 +6,47 @@ include "header.php";
 ?>
 
 
+<?php
+	if($_SERVER['REQUEST_METHOD'] == 'POST')
+	{
+		foreach($_POST as $key => $value)
+		{
+			if(strpos($key, 'carDetails') === 0)
+			{
+				$currentCar_id = intval(substr($key, 10));
+				//Do the display of the car
+			}
+		}
+	}
+	else
+	{
+		echo "Request method not supported. Please user our main page";
+	}
+	//var_dump($_POST['id']);
+	/*if(isset($_POST['carDetails']))
+		{
+			echo "<img src='' alt='No Image Available' class='img-rounded'/>";
+			echo "<br/><div align='right'><font size='1'><a href='#'>Add Img...</a></font></div>";
+		}
+	else{echo "Request method not supported";}*/
+	
+	$currentCar=mysql_query("SELECT *, make, model, car_condition_name FROM car
+									INNER JOIN make_id ON car.make_id = make_id.make_id
+									INNER JOIN model_id ON car.model_id = model_id.model_id
+									INNER JOIN car_condition ON car.condition_id = car_condition.id_car_condition
+									WHERE status_id=1 AND car_id='".$currentCar_id."'");
+														
+	$currentPics=mysql_query("SELECT pictures_path FROM car_pictures
+												WHERE car_pictures.car_id='".$currentCar_id."'");													
+														
+	//var_dump($currentCar);
+	$_POST['currentCarDB'] = mysql_fetch_array($currentCar);
+	$_POST['currentCarPics'] = mysql_fetch_array($currentPics);
+	//var_dump($_POST['currentCarDB']['car_id']);
+	//var_dump($_POST['currentCarPics']);
+	//var_dump($_POST['currentCarPics']['pictures_path']);
+?>	
+
 <html>
 <body>
 	<link href = "css/bootstrap.min.css" rel = "stylesheet">
@@ -14,21 +55,35 @@ include "header.php";
 		<th>
 			<td colspan='5' align='center'><h3 class = "panel-heading">Car Details</h3></td>
 		</th>
+		
 		<tr>
+				
 			<td width='3%'></td>
-			<td width='350' height='300' align='center' valign='middle'>
-				<img src='' alt='No Image Available' class="img-rounded"/>
-				<?php
-					var_dump($_POST['carDetails']);
-					if(isset($_POST['carDetails']))
-						{
-							echo "<img src='' alt='No Image Available' class='img-rounded'/>";
-							echo "<br/><div align='right'><font size='1'><a href='#'>Add Img...</a></font></div>";
-						}
-					else{echo "Request method not supported";}
-				?>						
-			</td>
-			<td width='3%'></td>
+			
+			
+				<td class="col-sm-14 col-md-7">
+				  <div class="thumbnail">
+					 <img src="<?php echo $_POST['currentCarPics']['pictures_path'];?>" 
+					 alt="Generic placeholder thumbnail">
+				  </div>
+				  <div class="caption">
+					 <h2>
+						<?php echo "".$_POST['currentCarDB']['year']." ".$_POST['currentCarDB']['make']." ".$_POST['currentCarDB']['model']."";?> 
+						
+						<a href="#" align = "center" class="btn btn-primary pull-right" role="button">
+						   Buy Now
+						</a> 
+						
+						
+					 </h2>
+					 <p></p>
+					 <p>
+						
+						
+					 </p>
+				  </div>
+			   </td>
+			
 			<td valign='top'>
 				<fieldset class='divFormatHeader'><legend>Vehicle Specifications</legend>
 				<table border='0' width='100%' name='orgCarDetails' style='font-size:12'>
@@ -74,16 +129,140 @@ include "header.php";
 			<td width='3%'></td>
 		</tr>
 		<tr>
-			<td colspan='5'>PICTURES:
+			<td colspan='5'>
 				<br/><br/>
 			</td>
 		</tr>
 		<tr>
-			<td colspan='5'>Additional Details:
+			<td colspan='5'>
 				<br/><br/>
 			</td>
 		</tr>
 	</table>
+
+	 <!--
+
+ Carousel
+    ================================================== 
+
+-->
+
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+
+    <!--
+
+     Indicators 
+
+    -->
+    <ol class="carousel-indicators">
+        <li class="" data-slide-to="0" data-target="#myCarousel"></li>
+        <li class="active" data-slide-to="1" data-target="#myCarousel"></li>
+        <li class="" data-slide-to="2" data-target="#myCarousel"></li>
+    </ol>
+    <div class="carousel-inner">
+        <div class="item">
+            <img alt="First slide" data-src="holder.js/900x500/auto/#777:#7a7a7a/text:First slide" src="data:image/svg+xml;base64,PHN2…lyc3Qgc2xpZGU8L3RleHQ+PC9zdmc+"></img>
+            <div class="container">
+                <div class="carousel-caption">
+                    <h1>
+
+                        Example headline.
+
+                    </h1>
+                    <p>
+
+                        Note: If you're viewing this page via a 
+
+                        <code>
+
+                            file://
+
+                        </code>
+
+                         URL, the "next" and "previous" Glyphicon buttons …
+
+                    </p>
+                    <p>
+                        <a class="btn btn-lg btn-primary" role="button" href="#">
+
+                            Sign up today
+
+                        </a>
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="item active">
+            <img alt="Second slide" data-src="holder.js/900x500/auto/#666:#6a6a6a/text:Second slide" src="data:image/svg+xml;base64,PHN2…5kIHNsaWRlPC90ZXh0Pjwvc3ZnPg=="></img>
+            <div class="container">
+                <div class="carousel-caption">
+                    <h1>
+
+                        Another example headline.
+
+                    </h1>
+                    <p>
+
+                        Cras justo odio, dapibus ac facilisis in, egestas …
+
+                    </p>
+                    <p>
+                        <a class="btn btn-lg btn-primary" role="button" href="#">
+
+                            Learn more
+
+                        </a>
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="item">
+            <img alt="Third slide" data-src="holder.js/900x500/auto/#555:#5a5a5a/text:Third slide" src="data:image/svg+xml;base64,PHN2…hpcmQgc2xpZGU8L3RleHQ+PC9zdmc+"></img>
+            <div class="container">
+                <div class="carousel-caption">
+                    <h1>
+
+                        One more for good measure.
+
+                    </h1>
+                    <p>
+
+                        Cras justo odio, dapibus ac facilisis in, egestas …
+
+                    </p>
+                    <p>
+                        <a class="btn btn-lg btn-primary" role="button" href="#">
+
+                            Browse gallery
+
+                        </a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <a class="left carousel-control" data-slide="prev" href="#myCarousel">
+        <span class="glyphicon glyphicon-chevron-left"></span>
+    </a>
+    <a class="right carousel-control" data-slide="next" href="#myCarousel">
+        <span class="glyphicon glyphicon-chevron-right"></span>
+    </a>
+
+</div>
+<!--
+
+ /.carousel 
+
+-->
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+
+<script src="../../dist/js/bootstrap.min.js"></script>
+
+<script src="../../assets/js/docs.min.js"></script>
+
+
+
 
 </body>
 </html>
